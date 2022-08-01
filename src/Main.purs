a722d2816eb95f412e84e0865a6c3db640d2e1eb
Mark Tang <MarkTangCd@hotmail.com>
2022-08-01 18:31:17 +0800

    done

--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,8 @@ module Main where
 
 import Prelude
 
-import Effect (Effect)
+import Euler (answer)
 import Effect.Console (log)
 
-main :: Effect Unit
 main = do
-  log "🍝"
+  log ("The answer is " <> show answer)
--- test/Main.purs
+++ test/Main.purs
@@ -2,10 +2,8 @@ module Test.Main where
 
 import Prelude
 
-import Effect (Effect)
-import Effect.Class.Console (log)
+import Euler (answer)
+import Test.Assert (assert)
 
-main :: Effect Unit
 main = do
-  log "🍝"
-  log "You should add some tests."
+  assert (answer == 233168)
